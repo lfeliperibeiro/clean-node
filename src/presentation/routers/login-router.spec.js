b853cb405230111e53ab77a6da9843a5a5edf411
Luiz Felipe Ribeiro <piporibeiro@gmail.com>
2020-09-17 13:45:45 -0300

    ensure LoginRouter retunrs 400 if no password is provider

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,6 +1,6 @@
 class LoginRouter {
   route (httpRequest) {
-    if (!httpRequest.body.email) {
+    if (!httpRequest.body.email || !httpRequest.body.password) {
       return {
         statusCode: 400
       }
@@ -14,6 +14,17 @@ describe('Login Router', () => {
     const httpRequest = {
       body: {
         password: 'any_password'
+      }
+    }
+    const httpRepsonse = sut.route(httpRequest)
+    expect(httpRepsonse.statusCode).toBe(400)
+  })
+
+  test('Shold return 400 if no email is provider', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        password: 'any_email@email.com'
       }
     }
     const httpRepsonse = sut.route(httpRequest)
