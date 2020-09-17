d45bfafebd97e95e5c515078df7f762d1143739b
Luiz Felipe Ribeiro <piporibeiro@gmail.com>
2020-09-17 16:36:44 -0300

    ensure LoginRouter returns a custom error if no email or password are provider

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,16 +1,38 @@
 class LoginRouter {
   route (httpRequest) {
     if (!httpRequest || !httpRequest.body) {
-      return {
-        statusCode: 500
-      }
+      return HttpRepsonse.serverError()
     }
     const { email, password } = httpRequest.body
-    if (!email || !password) {
-      return {
-        statusCode: 400
-      }
+    if (!email) {
+      return HttpRepsonse.badRequest('email')
     }
+    if (!password) {
+      return HttpRepsonse.badRequest('password')
+    }
+  }
+}
+
+class HttpRepsonse {
+  static badRequest (paramName) {
+    return {
+      statusCode: 400,
+      body: new MissingParamError(paramName)
+
+    }
+  }
+
+  static serverError () {
+    return {
+      statusCode: 500
+    }
+  }
+}
+
+class MissingParamError extends Error {
+  constructor (paramName) {
+    super(`Missing param: ${paramName}`)
+    this.name = 'MissingParamError'
   }
 }
 
@@ -22,28 +44,30 @@ describe('Login Router', () => {
         password: 'any_password'
       }
     }
-    const httpRepsonse = sut.route(httpRequest)
-    expect(httpRepsonse.statusCode).toBe(400)
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('email'))
   })
 
   test('Shold return 400 if no email is provider', () => {
     const sut = new LoginRouter()
     const httpRequest = {
       body: {
-        password: 'any_email@email.com'
+        email: 'any_email@email.com'
       }
     }
-    const httpRepsonse = sut.route(httpRequest)
-    expect(httpRepsonse.statusCode).toBe(400)
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('password'))
   })
   test('Shold return 500 if no httpRequest is provider', () => {
     const sut = new LoginRouter()
-    const httpRepsonse = sut.route()
-    expect(httpRepsonse.statusCode).toBe(500)
+    const httpResponse = sut.route()
+    expect(httpResponse.statusCode).toBe(500)
   })
   test('Shold return 500 if no httpRequest has no body', () => {
     const sut = new LoginRouter()
-    const httpRepsonse = sut.route({})
-    expect(httpRepsonse.statusCode).toBe(500)
+    const httpResponse = sut.route({})
+    expect(httpResponse.statusCode).toBe(500)
   })
 })
